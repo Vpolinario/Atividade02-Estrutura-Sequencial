aefe4880faeec51d03e612c12459f3a967ea52e8
Vinícius Alves <vpolinario2004@gmail.com>
2022-09-27 16:06:11 -0300

    primeiro commit

---
+++ atividade2.java
@@ -0,0 +1,22 @@
+package Atts_Montanha;
+
+import java.util.Scanner;
+
+public class att2 {
+	public static void main (String[] args){
+		Scanner entrada = new Scanner (System.in);
+		
+		double num;
+		
+		System.out.print("Informe Numero:");
+		num = entrada.nextDouble();
+		entrada.close();
+		
+		System.out.println("O numero informado foi: " + num);
+		
+		
+	}
+	
+	
+
+}
